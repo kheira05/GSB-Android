d24866c83a3e3fd89678c825a50f10a052440de2
kheira <kheira200@hotmail.com>
2021-05-04 04:14:15 +0200

    ui

---
+++ app/src/main/java/com/example/gsb_android/DAO.java
@@ -0,0 +1,105 @@
+package com.example.gsb_android;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class DAO {
+    private static final String urlmed = "http://gaemedecins.appspot.com/Controleur/medParDep/listeMed/";
+    private static final String urldep = "http://denis.brodard.free.fr/dep.php";
+    private static String[] LesInfos;
+
+    /**
+     *
+     * @param numdep
+     * @return LesInfos
+     */
+
+    public static ArrayList<Medecin> getLesInfos(String numdep){
+        ArrayList<Medecin> LesInfos = new ArrayList<Medecin>();
+        try{
+            URL myURL = new URL(urlmed + numdep);
+            Document doc;
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            doc = db.parse(myURL.openStream());
+            Element racine = doc.getDocumentElement();
+            NodeList listeMed = racine.getElementsByTagName("Medecin");
+            // récup des médecins
+            System.out.println("listemed: "+ listeMed.getLength());
+            for (int i = 0; i < 5; i++) {
+                Node medecin = listeMed.item(i);
+                NodeList lesProprietes = medecin.getChildNodes();
+                System.out.println("propp: "+ lesProprietes.toString());
+                // recherche du nom
+                Medecin unmed = new Medecin();
+                System.out.println("propro: "+ lesProprietes.getLength());
+                for (int j = 0; j < lesProprietes.getLength(); j++) {
+                    switch (lesProprietes.item(j).getNodeName()){
+                        case "nom":unmed.setmNom(lesProprietes.item(j).getTextContent().trim());
+                        break;
+                        case "prenom":unmed.setmPrenom(lesProprietes.item(j).getTextContent().trim());
+                        break;
+                        case "adresse":unmed.setmAdresse(lesProprietes.item(j).getTextContent().trim());
+                        break;
+                        case "specialite":unmed.setmSpecialite(lesProprietes.item(j).getTextContent().trim());
+                        break;
+                        case "tel":unmed.setmTel(lesProprietes.item(j).getTextContent().trim());
+                        break;
+                    }
+                }
+                LesInfos.add(unmed);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return LesInfos;
+    }
+
+
+
+    /**
+     *
+     * @return LesDeps
+     */
+    public static ArrayList<String> getLesDeps(){
+        ArrayList<String> LesDeps = new ArrayList<String>();
+        try{
+            URL myURL = new URL(urldep);
+            Document doc;
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            doc = db.parse(myURL.openStream());
+            Element racine = doc.getDocumentElement();
+            NodeList listeDep = racine.getElementsByTagName("Departement");
+            // récup des dep
+            for (int i = 0; i < listeDep.getLength(); i++) {
+                Node departement = listeDep.item(i);
+                NodeList lesProprietes = departement.getChildNodes();
+                // recherche du nom
+                for (int j = 0; j < lesProprietes.getLength(); j++) {
+                    if (lesProprietes.item(j).getNodeName().equals("num")) {
+                        LesDeps.add(lesProprietes.item(j).getTextContent().trim());
+                        System.out.println("eeeeeee");
+                        System.out.println(lesProprietes.item(j).getTextContent().trim());
+                        break;
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return LesDeps;
+    }
+}
---
+++ app/src/main/java/com/example/gsb_android/MainActivity2.java
@@ -0,0 +1,51 @@
+package com.example.gsb_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity2 extends AppCompatActivity {
+    private ArrayList<Medecin> lesMed;
+    private String departement;
+
+    @Override
+    /**
+     *
+     */
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        departement = (String) getIntent().getSerializableExtra("dep");
+        new Lismed().execute();
+    }
+
+
+
+    /**
+     *
+     */
+    private class Lismed extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] params) {
+            lesMed = DAO.getLesInfos(departement);
+            return null;
+        }
+
+        /**
+         *
+         * @param o
+         */
+        protected void onPostExecute(Object o) {
+            ListView listeMed = findViewById(R.id.Med);
+            listeMed.setAdapter(new Adaptermed(MainActivity2.this,lesMed));
+        }
+    }
+}
